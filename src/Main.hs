e1f7062a8ba6b72d3b7af7a0325534c15f2558b1
Lef Ioannidis <elefthei@mit.edu>
2019-02-20 13:16:27 -0500

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE StandaloneDeriving #-}
+module Main where
+import Data.Text (Text)
+import qualified Data.Text as T
+
+data Expr a where
+    Var :: Text -> Expr a
+    Lam :: Text -> Expr a -> Expr a
+    App :: Expr a -> Expr a -> Expr a
+
+deriving instance Show (Expr a)
+deriving instance Eq a => Eq (Expr a)
+
+
+subst :: Expr a -> Text -> Expr a -> Expr a
+subst (Lam b exp) v arg
+    | v == b    = Lam b exp -- Shadowing
+    | otherwise = Lam b $ subst exp v arg
+subst (App l a) v arg = App (subst l v arg) (subst a v arg)
+subst (Var a) v arg
+    | a == v = arg
+    | otherwise = Var a
+
+eval :: Expr a -> Expr a
+eval (App (Lam v exp) arg) = subst (eval exp) v (eval arg)
+    where next = eval exp
+eval (App f e) = App (eval f) (eval e)
+eval (Lam v e) = Lam v $ eval e
+eval e = e
+
+main :: IO ()
+main = putStrLn . show . eval $ App (Lam "x" (Var "x")) (Var "3")
